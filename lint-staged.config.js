253dc50db23e32edfaf4919c8491d03f35c95da5
Delave-las-Kure <t1evgeny93@gmail.com>
2021-04-19 20:47:03 +0300

    removed log

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,8 @@ module.exports = {
   '**/*.{js,jsx,ts,tsx,md,css,sass,scss}': (filenames) =>
     filenames.map((filename) => `prettier --write '${filename}'`),
   // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': (filenames) => (
-    console.log(filenames),
-    filenames.map((filename) => `npx tsc ${filename} --pretty --noEmit`)
-  ),
+  '**/*.ts?(x)': (filenames) =>
+    filenames.map((filename) => `npx tsc ${filename} --pretty --noEmit`),
   // Run ESLint on changes to JavaScript/TypeScript files
   '**/*.(ts|js)?(x)': (filenames) => `npm run lint ${filenames.join(' ')}`,
 }
